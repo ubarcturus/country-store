346254ba5f533d38dc379e8805573f23fa11a513
ubarcturus <ubarcturus@gmail.com>
2020-01-09 11:30:06 +0100

    add response for html and json

--- express.js
+++ express.js
@@ -1,5 +1,20 @@
-constexpress = require('express');
-constapp = express();
-app.listen(3333, () => {
-  console.log('server is listening on port 3333');
+const express = require('express');
+const path = require('path');
+const products = require('./beispiel-assets/products.json');
+const app = express();
+const port = 3333;
+
+app.use("/", express.static(path.join(__dirname, 'beispiel-assets')));
+
+app.listen(port, () => {
+  console.log(`server is listening on port ${port}`);
 });
+
+app.get('/', (request, response) => {
+  response.sendFile(path.join(__dirname, 'overview.html'));
+  response.json(products);
+});
+
+/* app.get('/', (req, res) => {
+  res.send(`The resource '/' has been requested`);
+}); */
