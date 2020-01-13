84195c850cf79765714251bd2124f9b5f58b8833
ubarcturus <ubarcturus@gmail.com>
2020-01-13 12:53:57 +0100

    Add all pages to frontend

--- express.js
+++ express.js
@@ -3,16 +3,27 @@ const path = require('path');
 const products = require('./beispiel-assets/products.json');
 const app = express();
 const port = 3333;
+const __dirname = '';
 
-app.use("/", express.static(path.join(__dirname, 'beispiel-assets')));
+app.use('/', express.static(path.join(__dirname, 'beispiel-assets')));
 
 app.listen(port, () => {
   console.log(`server is listening on port ${port}`);
 });
 
+// frontend
 app.get('/', (request, response) => {
   response.sendFile(path.join(__dirname, 'overview.html'));
   response.json(products);
+});
+app.get('/detailed_view', (request, response) => {
+  response.sendFile(path.join(__dirname, 'detailed_view.html'));
+});
+app.get('/checkout', (request, response) => {
+  response.sendFile(path.join(__dirname, 'checkout.html'));
+});
+app.get('/shopping_cart', (request, response) => {
+  response.sendFile(path.join(__dirname, 'shopping_cart.html'));
 });
 
 /* app.get('/', (req, res) => {
